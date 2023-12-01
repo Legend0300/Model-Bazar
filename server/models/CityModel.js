8ba00d317b90c3ae18278c1e2badf501a80ab0f8
Ali Hassan <58529870+mhassa204@users.noreply.github.com>
2023-12-01 15:57:21 +0500

    Update CityModel.js

--- server/models/CityModel.js
+++ server/models/CityModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const citySchema = new mongoose.Schema({
-    province: {
+
+    name: {
         type: String,
         required: true
     },
@@ -9,7 +10,7 @@ const citySchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    name: {
+    province: {
         type: String,
         required: true
     },
