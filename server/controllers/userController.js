f447c623745a2e747e8d0af47359f30a75321a0a
Legend0300 <ahmed.mansur.bhatti@address.com>
2023-12-02 20:14:01 +0500

    added DB connection and made structural changes

---
+++ server/controllers/userController.js
@@ -0,0 +1,82 @@
+
+const User = require('../models/UserModel'); // Correct the casing here
+
+// Get all users
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get a user by ID
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Add a new user
+const addUser = async (req, res) => {
+  const user = new User(req.body);
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Delete a user
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      await user.remove();
+      res.json({ message: 'User deleted' });
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Update a user
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      user.username = req.body.username || user.username;
+      user.email = req.body.email || user.email;
+      user.password = req.body.password || user.password;
+      user.phone = req.body.phone || user.phone;
+      user.status = req.body.status || user.status;
+      user.userType = req.body.userType || user.userType;
+
+      const updatedUser = await user.save();
+      res.json(updatedUser);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  addUser,
+  deleteUser,
+  updateUser
+};
