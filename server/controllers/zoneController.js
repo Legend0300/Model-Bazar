553aa3b0b05cd521a6c6ff14494076b5e136e965
MAS191 <m.abdullahshahid2003@gmail.com>
2023-12-02 21:41:54 +0500

    VERY MAJOR bug fixes

---
+++ server/controllers/zoneController.js
@@ -0,0 +1,70 @@
+const Zone = require('../models/ZoneModel');
+
+// Create a new zone
+const createZone = async (req, res) => {
+  try {
+    const zone = await Zone.create(req.body);
+    res.status(201).json(zone);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create zone' });
+  }
+};
+
+// Get all zones
+const getZones = async (req, res) => {
+  try {
+    const zones = await Zone.find();
+    res.json(zones);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to get zones' });
+  }
+};
+
+// Get a single zone by ID
+const getZoneById = async (req, res) => {
+  try {
+    const zone = await Zone.findById(req.params.id);
+    if (!zone) {
+      return res.status(404).json({ error: 'Zone not found' });
+    }
+    res.json(zone);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to get zone' });
+  }
+};
+
+// Update a zone by ID
+const updateZoneById = async (req, res) => {
+  try {
+    const zone = await Zone.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!zone) {
+      return res.status(404).json({ error: 'Zone not found' });
+    }
+    res.json(zone);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update zone' });
+  }
+};
+
+// Delete a zone by ID
+const deleteZoneById = async (req, res) => {
+  try {
+    const zone = await Zone.findByIdAndDelete(req.params.id);
+    if (!zone) {
+      return res.status(404).json({ error: 'Zone not found' });
+    }
+    res.json({ message: 'Zone deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to delete zone' });
+  }
+};
+
+module.exports = {
+  createZone,
+  getZones,
+  getZoneById,
+  updateZoneById,
+  deleteZoneById,
+};
--- server/routes/cityRoutes.js
+++ server/routes/cityRoutes.js
@@ -2,21 +2,21 @@
 const express = require('express');
 const router = express.Router();
 const CityModel = require('../models/CityModel');
-const cityController = require('../controllers/cityController');
+const  {getAllCities, getCityById, createCity, updateCityById, deleteCityById} = require('../controllers/cityController');
 
 // GET all cities
-router.get('/', cityController.getAllCities);
+router.get('/',  getAllCities);
 
 // GET a specific city
-router.get('/:id', cityController.getCityById);
+router.get('/:id',  getCityById);
 
 // POST a new city
-router.post('/', cityController.createCity);
+router.post('/',  createCity);
 
 // PATCH an existing city
-router.patch('/:id', cityController.updateCity);
+router.patch('/:id', updateCityById);
 
 // DELETE a city
-router.delete('/:id', cityController.deleteCity);
+router.delete('/:id',  deleteCityById);
 
 module.exports = router;
