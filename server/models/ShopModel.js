5014177c50dd2a5c3be9f4ed34fc2594ce3c57dc
Legend0300 <ahmed.mansur.bhatti@address.com>
2023-12-03 22:01:07 +0500

    fixed bug

--- server/models/ShopModel.js
+++ server/models/ShopModel.js
@@ -55,12 +55,10 @@ const shopSchema = new mongoose.Schema({
 // Define models
 const StallCategory = mongoose.model('StallCategory', shopCategorySchema);
 const ShopType = mongoose.model('ShopType', shopTypeSchema);
-const IncomeCategory = mongoose.model('IncomeCategory', incomeCategorySchema);
 const Shop = mongoose.model('Shop', shopSchema);
 
 module.exports = {
     StallCategory,
     ShopType,
-    IncomeCategory,
     Shop
 };
