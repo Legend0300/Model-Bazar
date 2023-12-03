fe67affea3370d89ec14a788fe49c6387565b865
Legend0300 <ahmed.mansur.bhatti@address.com>
2023-12-03 14:26:15 +0500

    added error handling

--- server/server.js
+++ server/server.js
@@ -1,30 +1,26 @@
 // server.js
 const express = require('express');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const connect = require('./config/dbConnection');
-// const superadminRoutes = require('./routes/superadmin');
 const userRoutes = require('./routes/userRoutes');
 const zoneRoutes = require('./routes/zoneRoutes');
 const bazarRoutes = require('./routes/bazarRoutes');
 const cityRoutes = require('./routes/cityRoutes');
 const shopRoutes = require('./routes/shopRouters');
 const shopHolderRoutes = require('./routes/shopHolderRoutes');
+const errorHandler = require("./middleware/errorHandler");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// MongoDB Connection
-// mongoose.connect(process.env.MONGODB_URI);
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// db.once('open', () => console.log('Connected to MongoDB'));
 
 // connect();
 
 app.use(express.json());
+app.use(errorHandler);
+
 
 // Define your routes here
 app.use('/users', userRoutes);
